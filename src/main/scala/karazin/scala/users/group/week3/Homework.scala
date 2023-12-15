c558b13843a7cded3b19b977abb7b2376a966c8d
mykola <nik.gora.2004@gmail.com>
2023-12-15 16:59:23 +0100

    week-3 - some code in Succ

--- src/main/scala/karazin/scala/users/group/week3/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week3/Homework.scala
@@ -1,5 +1,7 @@
 package karazin.scala.users.group.week3
 
+import scala.annotation.tailrec
+
 object Homework:
 
   type Zero = Zero.type
@@ -20,29 +22,44 @@ object Homework:
     def toInt: Int
 
     // Optional task
-    def fromInt(int: Int) = ???
+    def fromInt(int: Int) =
+      @tailrec()
+      def fromIntRec(int: Int,  suc: Nat):Nat =
+        if (int == 0) suc
+        else fromIntRec(int-1, successor)
 
+      fromIntRec(int, Zero)
     override def toString: String = s"Nat($predecessor)"
 
   class Succ(n: Nat) extends Nat:
-    def isZero: Boolean = false
-
-    def predecessor: Nat = ???
-
     infix def +(that: Nat): Nat = ???
 
     infix def -(that: Nat): Nat = ???
 
     // Optional task
-    def toInt: Int = ???
+    def toInt: Int = 1 + predecessor.toInt
 
     override def toString = s"Succ()"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(other: Any): Boolean = other match
+      case that: Succ =>
+        that.canEqual(this) &&
+          hashCode() == that.hashCode() &&
+          isZero == that.isZero &&
+          predecessor == that.predecessor
+      case _ => false
+
+    private def canEqual(other: Any): Boolean = other.isInstanceOf[Succ]
+
+    override def hashCode(): Int =
+      val state = Seq(isZero, predecessor)
+      state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+
+    def isZero: Boolean = false
+
+    def predecessor: Nat = n
 
   object Zero extends Nat:
-    def isZero: Boolean = true
-
     def predecessor: Nat = throw new Exception("0 doesn't have a predecessor")
 
     infix def +(that: Nat): Nat = that
@@ -57,13 +74,14 @@ object Homework:
     override def equals(other: Any): Boolean = other match
       case that: Zero =>
         that.canEqual(this) &&
+          hashCode() == that.hashCode() &&
           isZero == that.isZero
       case _ => false
+
+    def isZero: Boolean = true
 
     private def canEqual(other: Any): Boolean = other.isInstanceOf[Zero]
 
     override def hashCode(): Int =
       val state = Seq(isZero)
       state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
-
-
